8baace6214514bf6b4293a71e8ba2fe6a41cc15a
qiuey00 <qiuey00@gmail.com>
2019-02-16 16:05:33 -0500

    i jsut did stuffs to try to make stuffs

---
+++ app/src/main/java/com/example/jeff/butterfly/Fragments/SocialFeedFragment.java
@@ -0,0 +1,48 @@
+package com.example.jeff.butterfly.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.jeff.butterfly.Helpers.DbHelper;
+import com.example.jeff.butterfly.R;
+import com.example.jeff.butterfly.RecyclerViewAdapter;
+
+public class SocialFeedFragment extends Fragment {
+
+	private DbHelper db;
+	private AppCompatActivity activity;
+    private View view;
+    private RecyclerView feed;
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        db = new DbHelper(this.getActivity());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.socialpage, container, false);
+    }
+    
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        view = getView();
+        activity = (AppCompatActivity) getActivity();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        feed = view.findViewById(R.id.socialFeed);
+        feed.removeAllViews();
+//        RecyclerViewAdapter adapter = new RecyclerViewAdapter(activity, db.getAllTransactions());
+//        feed.setAdapter(adapter);
+//        feed.setLayoutManager(new LinearLayoutManager(activity));
+    }
+}
