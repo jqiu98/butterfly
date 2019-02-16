f2ea752a045af488fa0fcaa817647ea317a5dcaf
qiuey00 <qiuey00@gmail.com>
2019-02-16 14:05:54 -0500

    created fragment manager and container

--- app/src/main/java/com/example/jeff/butterfly/MainActivity.java
+++ app/src/main/java/com/example/jeff/butterfly/MainActivity.java
@@ -4,51 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import com.example.jeff.butterfly.Fragments.LandingFragment;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
-    RecyclerViewAdapter adapter;
-
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-//        mTextMessage = (TextView) findViewById(R.id.message);
-//        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
+        setContentView(R.layout.initpage);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, new LandingFragment(), "main")
+                .addToBackStack(null)
+                .commit();
     }
 
 
